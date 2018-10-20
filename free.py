6217d558af6e020e0b80e5fd6c0d61b961aca3b3
Pol Mesalles <pol.developer1@gmail.com>
2018-10-20 16:15:03 +0200

    Changes trying to make event callbacks work

--- free.py
+++ free.py
@@ -1,4 +1,5 @@
 # Init
+# import FreeCAD
 import Leap, sys, thread, time
 # from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
@@ -6,10 +7,10 @@ class FreeListener(Leap.Listener):
 	def on_init(self, controller):
 		self.isFirst = True
 		self.firstFrame = []
-		print "Initialized"
+		print("Initialized")
 
 	def on_connect(self, controller):
-		print "Connected"
+		print("Connected")
 
 		# Enable gestures
 		# controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
@@ -19,25 +20,25 @@ class FreeListener(Leap.Listener):
 
 	def on_disconnect(self, controller):
 		# Note: not dispatched when running in a debugger.
-		print "Disconnected"
+		print("Disconnected")
 
 	def on_exit(self, controller):
-		print "Exited"
+		print("Exited")
 
 	def on_frame(self, controller):
 		# Get the most recent frame and report some basic information
 		frame = controller.frame()
 
-		print "On Frame"
+		print("On Frame")
 
-		# print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+		# print("Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % ()
 			#   frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
 
 		# Get hands
 		if len(frame.hands) == 1:
-			print "1 hand"
+			print("1 hand")
 			if self.isFirst:
-				print "First"
+				print("First")
 				self.firstFrame = frame
 				self.isFirst = False
 			else:
@@ -45,18 +46,18 @@ class FreeListener(Leap.Listener):
 				axis = hand.rotation_axis(self.firstFrame)
 				angle = hand.rotation_angle(self.firstFrame)
 
-				axisCAD = FreeCAD.Vector(axis[0], axis[1], axis[2])
+				print(axis)
+				print(angle)
 
-				print axis
-				print angle
-
+				# axisCAD = FreeCAD.Vector(axis[0], axis[1], axis[2])
+				
 				# FreeCAD
-				rotation = FreeCAD.Rotation(axisCAD, angle)
-				Gui.activeView().setCameraOrientation(rotation)
+				# rotation = FreeCAD.Rotation(axisCAD, angle)
+				# Gui.activeView().setCameraOrientation(rotation)
 		else:
 			self.isFirst = True
 
-		Gui.updateGui()
+		# Gui.updateGui()
 
 def main():
 	listener = FreeListener()
@@ -65,8 +66,14 @@ def main():
 	# Have the listener receive events from the controller
 	controller.add_listener(listener)
 
+	# print("Running...")
+	# i = 0
+	# while True:
+	# 	i = i + 1
+	# 	# wait for shit to happen
+
 	# Keep this process running until Enter is pressed
-	print "Press Enter to quit..."
+	print("Press Enter to quit...")
 	try:
 		sys.stdin.readline()
 	except KeyboardInterrupt:
@@ -74,6 +81,8 @@ def main():
 	finally:
 		# Remove the sample listener when done
 		controller.remove_listener(listener)
+
+	controller.remove_listener(listener)
 
 if __name__ == "__main__":
 	main()
