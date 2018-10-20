540682f633ae320623c8ea76e0761ddc39af023e
Pol Mesalles <pol.developer1@gmail.com>
2018-10-21 00:25:11 +0200

    Add support for grip/grab strength 'gesture' to start motion

--- motion.py
+++ motion.py
@@ -7,6 +7,7 @@ import FreeCAD # pylint: disable = E0401
 
 # Constants
 DELAY = 0.1
+STRENGTH = 0.34
 
 class FreeController(threading.Thread):
 	def __init__(self):
@@ -32,35 +33,53 @@ class FreeController(threading.Thread):
 		# Initial variables
 		prev = controller.frame()
 		first = True
+		rt = []
 
 		# Loop
 		while self.running:
 			frame = controller.frame()
 
 			if frame.id > prev.id:
-				if len(frame.hands) == 1: # only accept 1 hand
+				if len(frame.hands) == 1: # we need a hand
 					hand = frame.hands[0]
 
-					if first:
-						first = False
+					print(hand.grab_strength)
 
-						# Basis 1
-						d1 = FreeCAD.Vector(hand.direction)
-						n1 = FreeCAD.Vector(hand.palm_normal)
+					if hand.grab_strength > STRENGTH:
+						if first:
+							first = False
+
+							# Basis 1
+							d1 = FreeCAD.Vector(hand.direction)
+							n1 = FreeCAD.Vector(hand.palm_normal)
+						else:
+							# Basis 2
+							d2 = FreeCAD.Vector(hand.direction)
+							n2 = FreeCAD.Vector(hand.palm_normal)
+
+							# Previous
+							rt0 = rt
+							
+							# Rotation
+							r1 = FreeCAD.Rotation(d1, d2)
+							nt = r1.multVec(n1)
+							r2 = FreeCAD.Rotation(nt, n2)
+							rt = r2.multiply(r1).inverted()
+
+							# Filter noise
+							try:
+								rtf = coin.SbRotation.slerp(rt0, rt, 0.5)
+							except:
+								rtf = rt
+							
+							# FreeCAD
+							FreeCAD.Gui.ActiveDocument.ActiveView.getCameraNode().orientation = coin.SbRotation(rtf.Q) * coin.SbRotation(r0)
+							# FreeCAD.Gui.SendMsgToActiveView("ViewFit") 
 					else:
-						# Basis 2
-						d2 = FreeCAD.Vector(hand.direction)
-						n2 = FreeCAD.Vector(hand.palm_normal)
-						
-						# Rotation
-						r1 = FreeCAD.Rotation(d1, d2)
-						nt = r1.multVec(n1)
-						r2 = FreeCAD.Rotation(nt, n2)
-						rt = r2.multiply(r1).inverted()
-						
-						# FreeCAD
-						FreeCAD.Gui.ActiveDocument.ActiveView.getCameraNode().orientation = coin.SbRotation(rt.Q) * coin.SbRotation(r0)
-						# FreeCAD.Gui.SendMsgToActiveView("ViewFit") 
+						if not first: # first == False
+							first = True
+							r0 = FreeCAD.Gui.ActiveDocument.ActiveView.getCameraNode().orientation.getValue().getValue()
+							FreeCAD.Gui.SendMsgToActiveView("ViewFit")
 				else:
 					if not first: # first == False
 						first = True
@@ -80,5 +99,6 @@ if __name__ == "__main__":
 		FreeCAD.t.start()
 		FreeCAD.Console.PrintMessage("Started LEAP Motion gesture reconition\n")
 	else:
+		FreeCAD.Gui.SendMsgToActiveView("ViewFit")
 		FreeCAD.t.stop()
 		FreeCAD.Console.PrintMessage("Stopped LEAP Motion gesture reconition\n")
