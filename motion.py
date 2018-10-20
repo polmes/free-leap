c05bc5d681cdf5f79e84a80f708717b8fa62009e
Pol Mesalles <pol.developer1@gmail.com>
2018-10-20 19:33:33 +0200

    Made it work

--- motion.py
+++ motion.py
@@ -2,6 +2,8 @@ import Leap
 import sys
 import threading
 import time
+# import numpy as np
+from pivy import coin
 import FreeCAD # pylint: disable = E0401
 
 # Constants
@@ -25,30 +27,60 @@ class FreeController(threading.Thread):
 	def main(self):
 		controller = Leap.Controller()
 
+		currentOrientation = FreeCAD.Gui.ActiveDocument.ActiveView.getCameraNode().orientation.getValue().getValue()
 		prevID = 0
 		isFirst = True
+		# firstFrame = []
+		d1 = []
+		n1 = []
 		while self.running:
 			frame = controller.frame()
 
 			if frame.id != prevID:
 				if len(frame.hands) == 1:
 					print("1 hand")
+					hand = frame.hands[0]
+
 					if isFirst:
 						isFirst = False
 						# firstFrame = frame
+						d1 = FreeCAD.Vector(hand.direction)
+						n1 = FreeCAD.Vector(hand.palm_normal)
 					else:
-						hand = frame.hands[0]
+						d2 = FreeCAD.Vector(hand.direction)
+						n2 = FreeCAD.Vector(hand.palm_normal)
+						
+						r1 = FreeCAD.Rotation(d1, d2)
+						nt = r1.multVec(n1)
+						r2 = FreeCAD.Rotation(nt, n2)
+						rt = r2.multiply(r1).inverted()
 
-						direction = hand.direction
-						normal = hand.palm_normal
+						print("rt = " + str(rt))
+						print("cO = " + str(currentOrientation))
 
-						print(direction)
-						print(normal)
-			
+						# print(direction)
+						# print(normal)
+						# print("dot: " + str(direction.dot(normal)))
+						
+						# FreeCAD
+						# FreeCAD.Gui.activeView().setCameraOrientation(rt)
+						FreeCAD.Gui.ActiveDocument.ActiveView.getCameraNode().orientation = coin.SbRotation(rt.Q) * coin.SbRotation(currentOrientation)
+						# FreeCAD.Gui.SendMsgToActiveView("ViewFit") 
+				else:
+					if not isFirst: # isFirst == False
+						isFirst = True
+						currentOrientation = FreeCAD.Gui.ActiveDocument.ActiveView.getCameraNode().orientation.getValue().getValue()
+						FreeCAD.Gui.SendMsgToActiveView("ViewFit")
+
 			FreeCAD.Gui.updateGui()
 			time.sleep(DELAY)
 
 if __name__ == "__main__":
+	try:
+		FreeCAD.t.stop()
+	except:
+		pass
+		
 	FreeCAD.t = FreeController()
 	FreeCAD.t.start()
 
